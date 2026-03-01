b53a2e16a387ca35ac00b02db84e286431ef4262
muhammadvallentinoakbar <muhammadvallentinoakbar@saintek.unidpu.ac.id>
2026-03-01 04:58:42 +0300

    feat: inisiasi project Laravel – muhammadvallentinoakbar

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/perkenalan', function () {
+
+    return '<h1>Halo! Nama saya Muhammad Vallentino Akbar</h1>
+
+            <p>NIM: 4124040 | Prodi: Sistem Informasi</p>
+
+            <p>Alhamdulillah Sedikit Banged Pak Pusingnya hehehe</p>
+
+            <p>Saya siap belajar Laravel! Tapi Pelan-Pelan Ya Pak heheheh 🚀</p>';
+
+            
+});
+    
