25c18a93338f2abcb07acffc4115fa9a3a312200
muhammadvallentinoakbar <muhammadvallentinoakbar@saintek.unidpu.ac.id>
2026-03-06 09:31:17 +0300

    Menambahkan route perkenalan

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route pertama - Vallentino
 Route::get('/perkenalan', function () {
 
     return '<h1>Halo! Nama saya Muhammad Vallentino Akbar</h1>
@@ -11,7 +12,15 @@ Route::get('/perkenalan', function () {
             <p>Alhamdulillah Sedikit Banged Pak Pusingnya hehehe</p>
 
             <p>Saya siap belajar Laravel! Tapi Pelan-Pelan Ya Pak heheheh 🚀</p>';
-
-            
 });
-    
+
+
+// Route kedua - Ubedilah
+Route::get('/perkenalan-ubed', function () {
+
+    return '<h1>Halo! Nama saya Ubedilah</h1>
+
+            <p>Bismillah Colaborasi</p>
+
+            <p>Saya siap belajar Laravel! Tapi Pelan-Pelan Ya Pak heheheh 🚀</p>';
+});
